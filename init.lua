e3ca9e78a6dcbce36cf4505257eef0453b860649
Kris Ezra <kris.ezra@gmail.com>
2021-05-05 15:18:25 -0400

    Addition of basic configurations.
     * Set the leader
     * Add convenience functions for interoperability
     * Automagically install packer.nvim
       * Packer is most analogous to vim-plug

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- LUA base configuration file
+-- Modified from: https://alpha2phi.medium.com/neovim-init-lua-e80f4f136030
+
+-- Set up interoperability with VimL
+local fn = vim.fn
+local execute = vim.api.nvim_command
+
+-- Configure leader
+vim.g.mapleader = '\\'
+
+-- Auto install packer.nvim if it doesn't exist
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+end
+vim.cmd [[packadd packer.nvim]]
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
