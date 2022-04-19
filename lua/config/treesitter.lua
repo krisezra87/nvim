773e16fa82ed06bac8e627f0afaaac09fd510746
Kris Ezra <kris.ezra@gmail.com>
2022-04-19 09:11:16 -0400

    Treesitter now uses only a subset of languages

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 -- Load up all the parsers
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "bash", "bibtex", "json", "json5", "lua", "python", "toml", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
   },
