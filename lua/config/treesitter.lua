674792a37e208dee66ef6368c9c5d3faddfea8e2
Kris Ezra <kris.ezra@gmail.com>
2022-06-24 23:26:25 -0400

    Specified treesitter languages

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 -- Load up all the parsers
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "bash", "bibtex", "json", "json5", "lua", "python", "toml", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"python","bash","bibtex","latex","regex","vim","json","json5","lua","toml"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
   },
