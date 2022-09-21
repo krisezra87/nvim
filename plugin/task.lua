2bc0fea5a51d8d665c994c2c492832125554f294
Kris Ezra <kris.ezra@gmail.com>
2022-09-21 15:51:04 -0400

    Fixup to add tags that don't exist if we search for them.

--- plugin/task.lua
+++ plugin/task.lua
@@ -27,16 +27,21 @@ vim.cmd([[command! FPR lua _G.fzf_project_replace()]])
 -- Replace a project title with a new one for taskwarrior
 _G.fzf_add_tag = function()
     -- First get us in the right place in the taskwarrior task file
-    vim.cmd([[exec "normal gg/Tags\<cr>zt$"]])
     local fzf_lua = require'fzf-lua'
     options = {}
     options.prompt = "Tags > "
     options.actions = {
         ['default'] = function(selected)
-            choice = selected[1]
+            vim.cmd([[exec "normal gg/Tags\<cr>zz$"]])
+            tag = nil
+            if #selected > 0 then
+                tag = selected[1]
+            else
+                tag = fzf_lua.config.__resume_data.last_query
+            end
             -- It seems to take us out of insert automagically, and doesn't
             -- even require a call to "exec" so just do the change
-            vim.cmd([[normal a ]] .. choice)
+            vim.cmd([[normal a ]] .. tag)
         end
     }
     --Make the call to get the projects
