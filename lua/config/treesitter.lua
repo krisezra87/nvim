778c6d515ce1bfd0869b1cf6cf5390fd22aaaacc
Kris Ezra <kris.ezra@gmail.com>
2024-07-10 15:35:02 -0400

    Removing extraneous filetypes from treesitter to save parser installation time

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,8 @@
 -- Load up all the parsers
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"python","bash","bibtex","latex","regex","vim","json","json5","lua","toml"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"python","bash","bibtex","latex","regex","vim","json","json5","toml"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
+    disable={'latex', 'lua'},
   },
 }
